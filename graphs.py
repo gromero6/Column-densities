b691293738ff4e675e9dfa54c0498e44688ab4e8
Gabriel <gromero6@miners.utep.edu>
2025-08-08 15:20:35 -0600

    graphs of different coordinates

--- graphs.py
+++ graphs.py
@@ -3,13 +3,23 @@ import numpy as np, h5py
 from scipy import spatial
 import matplotlib.pyplot as plt
 from library import *
-from los_stats_gabriel import seed, m, d
 
-data = np.load(f"thesis_los\ideal\430\DataBundle_MeanCD_{seed}_{m}_{d}.npz")
+seed  = input("Enter seed: ")
+m     = input("Enter number of points: ")
+d     = input("Enter Number of lines of sight per point: ")
+index = input("Enter index of the file to be read: ")
+case  = input("Enter case (ideal/amb): ")
+snap  = input("Enter snapshot number: ")
+
+data_directory = os.path.join("thesis_los", case, snap)
+data_name = f"DataBundle_MeanCD_{seed}_{m}_{d}_{index}.npz"
+full_data_path = os.path.join(data_directory, data_name)
+data = np.load(full_data_path)
+
 
 print(data.files)
-output_folder = ".\Column-densities\graphs"
-file_name = f"ColumnDensity_vs_RadialDistance_{seed}_{m}_{d}.png"
+output_folder = ".\graphs"
+png_name = f"ColumnDensity_vs_RadialDistance_{seed}_{m}_{d}_{index}.png"
 
 
 x_init = data['x_init_points']
@@ -30,8 +40,9 @@ plt.xlabel('$cm$', fontsize=14)
 plt.ylabel('$cm^{-2}$', fontsize=14)
 plt.title('Column Density vs. Distance', fontsize=16)
 
-plt.show()
-
-full_path = os.path.join(output_folder, file_name)
+full_path = os.path.join(output_folder, png_name)
 plt.savefig(full_path, dpi=300)
 print("plot saved to: ", full_path)
+
+plt.show()
+
